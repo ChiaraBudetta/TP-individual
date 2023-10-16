658236aae1a1b4c4481cca1f6e866b2bd5cf64f1
“Chiara <budettachiara@gmail.com>
2023-10-16 18:23:28 -0300

    correciones primer pre-entrega

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+// menu hamburguesa
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav").classList.toggle("show");
+});
+
+// menu hamburgesa para nav colecciones
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-colecciones").classList.toggle("show");
+});
